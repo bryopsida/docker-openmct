7826931bffe3e9fc2798af7e58340b9053d3635f
bryopsida <8363252+bryopsida@users.noreply.github.com>
2022-12-17 21:09:14 -0600

    add health check ep

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const Fastify = require('fastify')
 const FastifyStatic = require('@fastify/static')
+const FastifyUnderPressure = require('@fastify/under-pressure')
 const path = require('node:path')
 
 const fastify = Fastify({
   logger: true
+})
+
+fastify.register(FastifyUnderPressure, {
+  maxEventLoopDelay: process.env.FASTIFY_MAX_EVENT_LOOP_DELAY || 1000,
+  maxHeapUsedBytes: process.env.FASTIFY_MAX_HEAP_BYTES || 100000000,
+  maxRssBytes: process.env.FASTIFY_MAX_RSS_BYTES || 100000000,
+  maxEventLoopUtilization: process.env.FASTIFY_MAX_ || 0.98,
+  exposeStatusRoute: true
 })
 
 fastify.register(FastifyStatic, {
